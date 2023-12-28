e90039a4634a7908bc72442051a59980466d424e
Haitham <haitham.alrawi19@gmail.com>
2023-12-28 15:13:13 +0300

    created base for the cart functionality

---
+++ ECommerceFPE/ECommerceFPE/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using ECommerceFPE.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceFPE.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly ECommerceDBContext _context;
+
+    public AdminController(ECommerceDBContext context)
+    {
+        _context = context;
+    }
+
+    [Route("/admin/cart")]
+    public IActionResult Cart()
+    {
+        var cart = _context.Cart.ToList();
+
+        return View("~/Views/AdminDashboard/Cart.cshtml");
+    }
+}
