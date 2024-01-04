46ca7b59750870c1529830371deb688e735bfc4f
Hazem Salameh <148675673+HazemSalameh99@users.noreply.github.com>
2024-01-04 16:28:38 +0300

    الكود خرب عندي

---
+++ ECommerceFPE/ECommerceFPE/Migrations/20240104113913_AddCoulumn.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceFPE.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoulumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Product",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Order",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Category",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Category");
+        }
+    }
+}
